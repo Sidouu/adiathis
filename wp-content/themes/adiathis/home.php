fcaf4d2165dc07c6e1c095b620e13ab2f8dd0bf7
trax2hd <justin.asdrubal@gmail.com>
2014-03-17 15:23:19 +0100

    Modifications home template

--- wp-content/themes/adiathis/home.php
+++ wp-content/themes/adiathis/home.php
@@ -5,9 +5,25 @@
 
 get_header(); ?>
 <div class="wrap-main">
-	<div class="content">
-		
-	</div>
+	<section id="home-table">
+		<div class="clearfix bloc-img formations">
+			<div class="right bloc-color">
+				<h1>Formations</h1>
+				<span class="sep"></span>
+				<a href="formations" class="seeMore">en savoir +</a>
+			</div>
+		</div>
+		<div class="clearfix bloc-img coaching">
+			<div class="left bloc-color">
+				<h1>Coaching</h1>
+				<span class="sep"></span>
+				<a href="coaching" class="seeMore">en savoir +</a>
+			</div>
+		</div>
+
+		<span class="shadow-bloc"></span>
+	</section>
+	<div class="content"></div>
 </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
